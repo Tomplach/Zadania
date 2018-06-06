bb69d63afe31426635a3b1fff72062f7cc9d536d
Tomplach <tmchecki@gmail.com>
2018-06-06 11:24:33 +0200

    Create Chessboard.js

---
+++ Rozdzial-2/Chessboard.js
@@ -0,0 +1,14 @@
+let boardSize = 8;
+let board = "";
+
+for (let y = 0; y < boardSize; y++) {
+	for (let x = 0; x < boardSize; x++) {
+		if ((x+y) % 2 == 0) {
+			board += " ";
+		} else {
+			board += "#";
+		}
+	}
+	board += "\n";
+}
+console.log(board);
