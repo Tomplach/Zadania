702e73e2a4ea7306eae23cd041477432231fd661
Tomplach <tmchecki@gmail.com>
2018-06-12 15:01:43 +0200

    Create Minimum.js

---
+++ Rozdzial-3/Minimum.js
@@ -0,0 +1,11 @@
+function min(number1, number2) {
+  if (number1 > number2) {
+  	console.log(number2);
+  }
+  else if (number1 === number2) {
+  	console.log("These numbers are equal");
+  }
+  else {
+  	console.log(number1);
+  }
+}
