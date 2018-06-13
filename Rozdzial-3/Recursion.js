1dd1a2f89e0e901b90a70dff5b004f224fb1b40e
Tomplach <tmchecki@gmail.com>
2018-06-13 15:30:07 +0200

    Update Recursion.js

--- Rozdzial-3/Recursion.js
+++ Rozdzial-3/Recursion.js
@@ -1,14 +1,14 @@
-function isEven(n) {
-	if (n === 0) {
+function isEven(number) {
+	if (number == 0) {
   	return true;
   }
-  else if (n === 1) {
+  else if (number == 1) {
   	return false;
   }
-  else if (n < 0) {
-  	return isEven(-n);
+  else if (number < 0) {
+  	return isEven(-number);
   }
   else {
-  	return isEven(n - 2);
+  	return isEven(number - 2);
   }
 }
