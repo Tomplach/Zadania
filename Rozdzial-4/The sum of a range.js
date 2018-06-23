10e97e1443e4c5c4229db36b9055f6ec46b37361
Tomplach <tmchecki@gmail.com>
2018-06-23 19:19:32 +0200

    Update The sum of a range.js

--- Rozdzial-4/The sum of a range.js
+++ Rozdzial-4/The sum of a range.js
@@ -2,25 +2,25 @@ function range(start, end, jump = start < end ? 1 : -1) {
 	let array = [];
   
 	if (start < end && jump < 0) {
-  	return "You can not use a negative number (" + jump + ") to achieve an array with numbers from " + start + " to " + end;
-  } else if (start > end && jump > 0) {
-  	return "You can not use a positive number (" + jump + ") to achieve an array with numbers from " + start + " to " + end;
-  }
+  		return "You can not use a negative number (" + jump + ") to achieve an array with numbers from " + start + " to " + end;
+ 	} else if (start > end && jump > 0) {
+  		return "You can not use a positive number (" + jump + ") to achieve an array with numbers from " + start + " to " + end;
+  	}
   
-  if (jump > 0) {
- 		for (let i = start; i <= end; i += jump)
-  	array.push(i);
-  } else {
+	if (jump > 0) {
+		for (let i = start; i <= end; i += jump)
+  		array.push(i);
+  	} else {
  		for (let i = start; i >= end; i += jump)
-    array.push(i);
-  }
-  return array;
+		array.push(i);
+ 	}
+	return array;
 }
 
 function sum(array) {
 	let sumNum = 0;
  	for (let i of array) {
   	sumNum += i;
-  }
-  return sumNum;
+	}
+ 	return sumNum;
 }
