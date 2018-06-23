92c72171e6af8863658ae623a04f8c9490dc4e2a
Tomplach <tmchecki@gmail.com>
2018-06-23 19:15:31 +0200

    Create The sum of a range.js

---
+++ Rozdzial-4/The sum of a range.js
@@ -0,0 +1,26 @@
+function range(start, end, jump = start < end ? 1 : -1) {
+	let array = [];
+  
+	if (start < end && jump < 0) {
+  	return "You can not use a negative number (" + jump + ") to achieve an array with numbers from " + start + " to " + end;
+  } else if (start > end && jump > 0) {
+  	return "You can not use a positive number (" + jump + ") to achieve an array with numbers from " + start + " to " + end;
+  }
+  
+  if (jump > 0) {
+ 		for (let i = start; i <= end; i += jump)
+  	array.push(i);
+  } else {
+ 		for (let i = start; i >= end; i += jump)
+    array.push(i);
+  }
+  return array;
+}
+
+function sum(array) {
+	let sumNum = 0;
+ 	for (let i of array) {
+  	sumNum += i;
+  }
+  return sumNum;
+}
